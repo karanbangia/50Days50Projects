0e8bb652ce6bac9e33b24471a745450de81e7f58
karanbangia <karanbangia14@gmail.com>
2020-12-13 23:53:47 +0530

    code refactored

--- Day-4/Hidden-Search/script.js
+++ Day-4/Hidden-Search/script.js
@@ -1,9 +1,12 @@
 const search = document.querySelector('.search');
 const button = document.querySelector('.btn');
+const input = document.querySelector('.input');
 button.addEventListener('click', () => {
-    if (search.classList.contains('active')) {
-        search.classList.remove('active');
-    } else {
-        search.classList.add('active');
-    }
+    // if (search.classList.contains('active')) {
+    //     search.classList.remove('active');
+    // } else {
+    //     search.classList.add('active');
+    // }
+    search.classList.toggle('active');
+    input.focus();
 })
